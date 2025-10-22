07f3e85504a7a334b44e44c27fb67c353113a04d
frina <frinafrin149@gmail.com>
2025-10-21 13:12:14 -1200

    commmit

--- ai_server/modules/config.py
+++ ai_server/modules/config.py
@@ -11,7 +11,7 @@ import os
 # --- Model Configuration ---
 MODEL_NAME = "VGG-Face"
 # This is for high-quality single image uploads (face-search)
-CONFIDENCE_THRESHOLD = 0.70 
+CONFIDENCE_THRESHOLD = 0.65 
 # --- FINAL ADJUSTMENT: A more lenient threshold for real-time video ---
 # We are seeing scores around 0.33, so let's set the bar just below that.
 LIVE_STREAM_CONFIDENCE_THRESHOLD = 0.30
